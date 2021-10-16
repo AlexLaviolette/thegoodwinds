04ffd8878426321328b0ec7d5c9e6e8b7643e6d5
Alex Laviolette <alex.laviolette92@gmail.com>
2021-10-16 16:59:36 -0400

    privacy policy

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -13,6 +13,14 @@
         This website is maintained by <a href="https://github.com/AlexLaviolette" target="_blank">Alex</a> and <a href="https://github.com/kravse" target="_blank">Jared</a>.
         We get our weather information from the free <a href="https://openweathermap.org/api" target="_blank">Open Weather API</a>. The code used in this site is open source, so feel free to contribute&nbsp;on&nbsp;<a href="https://github.com/TheGoodWinds" target="_blank">GitHub</a>.
       </p>
+      <div id="privacy-policy">
+        <h4>Privacy Policy</h4>
+        <p>
+          We don't store your data.
+
+          We don't even have a database to store it in. Even if Paul McBeth asks really really nicely, we don't have anything to show him.
+        </p>
+      </div>
     </section>
   </div>
 </template>
