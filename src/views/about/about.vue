7f09b996cffdab23742e7ca9144720290121ce30
kravse <jared@cohere.ai>
2021-10-17 16:59:41 -0400

    fix about page height stuff

--- src/views/about/about.vue
+++ src/views/about/about.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="about">
     <section>
-      <div class="title">
-        <router-link class="logo" :to="{name: 'home'}"><img src="horizontal-logo-blue.png"/></router-link>
+      <div>
+        <div class="title">
+          <router-link class="logo" :to="{name: 'home'}"><img src="horizontal-logo-blue.png"/></router-link>
+        </div>
+        <p>
+          GoodWinds is a disc-golf weather app to help you figure out the best times in the coming week to play disc golf.
+          Weather is chunked into blocks and ranked based on a blended analysis of <i>Wind</i>, <i>Temperature</i>, <i>Precipitation</i>,
+          and <i>Time of Day</i>.
+        </p>
+        <p>
+          This website is maintained by <a href="https://github.com/AlexLaviolette" target="_blank">Alex</a> and <a href="https://github.com/kravse" target="_blank">Jared</a>.
+          We get our weather information from the free <a href="https://openweathermap.org/api" target="_blank">Open Weather API</a>. The code used in this site is open source, so feel free to contribute&nbsp;on&nbsp;<a href="https://github.com/TheGoodWinds" target="_blank">GitHub</a>.
+        </p>
       </div>
-      <p>
-        GoodWinds is a disc-golf weather app to help you figure out the best times in the coming week to play disc golf.
-        Weather is chunked into blocks and ranked based on a blended analysis of <i>Wind</i>, <i>Temperature</i>, <i>Precipitation</i>,
-        and <i>Time of Day</i>.
-      </p>
-      <p>
-        This website is maintained by <a href="https://github.com/AlexLaviolette" target="_blank">Alex</a> and <a href="https://github.com/kravse" target="_blank">Jared</a>.
-        We get our weather information from the free <a href="https://openweathermap.org/api" target="_blank">Open Weather API</a>. The code used in this site is open source, so feel free to contribute&nbsp;on&nbsp;<a href="https://github.com/TheGoodWinds" target="_blank">GitHub</a>.
-      </p>
       <div id="privacy-policy">
         <h4>Privacy Policy</h4>
         <p>
